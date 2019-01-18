e80e7f20f108762efe24100df1185673ebfe2ca8
LAWSGROUP\hans.tsang <hans.tsang@lawsgroup.com>
2019-01-18 09:37:34 +0800

    First commit

---
+++ CoreUtil/MailSender.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZTAMPZ_EMAIL_SWF.CoreUtil;
+using System.Net.Mail;
+using System.Net;
+using System.Data;
+
+namespace ZTAMPZ_EMAIL_SWF
+{
+    public class MailSender
+    {
+        public static void Send(string to, string SmtpServer, string Username, string Password, string Port, string EnableSSL, string EncryptedMethod, string SenderAddress, string subject, string body, string MimeType, IDbCommand Command)
+        {
+            string[] recipients = to.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string errMessage = "";
+            MailMessage mm = new MailMessage();
+            try
+            {
+                string smtpServer = SmtpServer;
+                string sender = Username;
+                string password = Password;
+                string port = Port;
+                string enableSSl = EnableSSL;
+                string encryptedMethod = EncryptedMethod;
+                string senderAddress = SenderAddress;
+                string mimeType = MimeType;
+                for (int i = 0; i < recipients.Length; i++)
+                {
+                    try
+                    {
+                        mm.To.Add(new MailAddress(recipients[i].Trim()));
+                    }
+                    catch (Exception e2)
+                    {
+                        errMessage += e2.Message + Environment.NewLine;
+                    }
+                }
+                mm.Subject = subject;
+                mm.Body = body;
+                mm.IsBodyHtml = mimeType == "text/plain" ? false : true;
+                mm.From = new MailAddress(senderAddress);
+                SmtpClient smtp = new SmtpClient();
+                if (smtpServer != string.Empty)
+                    smtp.Host = smtpServer;
+                if (encryptedMethod != string.Empty)
+                    ServicePointManager.SecurityProtocol = encryptedMethod.ToUpper() == "TLS" ? SecurityProtocolType.Tls : SecurityProtocolType.Ssl3;
+                if (port != string.Empty)
+                    smtp.Port = Convert.ToInt32(port);
+                if (enableSSl != string.Empty)
+                    smtp.EnableSsl = enableSSl == "Y" ? true : false;
+                if (password != string.Empty)
+                    smtp.Credentials = new System.Net.NetworkCredential(sender, password);
+                Console.WriteLine("Sending Email......");
+
+                smtp.Send(mm);
+                Console.WriteLine("Email Sent.");
+            }
+            catch (System.Net.Mail.SmtpFailedRecipientException e3)
+            {
+                Console.Error.WriteLine(e3);
+
+                List<string> failedRecipients = null;
+                var es = e3 as SmtpFailedRecipientsException;
+                if (es != null)
+                {
+                    errMessage = Helper.ConcatStringList(es.InnerExceptions.Select(ie => ie.Message), "\r\n");
+                    failedRecipients = es.InnerExceptions.Select(ie => ie.FailedRecipient).ToList();
+                }
+                else
+                {
+                    errMessage = e3.Message;
+                    if (e3 is SmtpFailedRecipientException)
+                    {
+                        failedRecipients = new List<string>() { ((SmtpFailedRecipientException)e3).FailedRecipient };
+                    }
+                }
+
+                if (mm.To.Count == failedRecipients.Count)
+                {
+                    throw;
+                }
+                else
+                {
+                    DatabaseHelper.LogError(Command, e3);
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ RSSFetcher/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.Data.SQLite;
+
+namespace RSSFetcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://hk.jobsdb.com/HK/en/Rss/JobListing?jobFunctionId=131";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                String ID = item.Id;
+                String subject = item.Title.Text;
+                String summary = item.Summary.Text;
+                String links = item.Links[0].Uri.OriginalString;
+                DateTime publishDate = item.PublishDate.LocalDateTime;
+                //Console.WriteLine(ID);
+                //Console.WriteLine(subject);
+                //Console.WriteLine(summary);
+                //Console.WriteLine(links);
+                //Console.WriteLine(publishDate);
+
+                SQLiteConnection m_dbConnection;
+                m_dbConnection = new SQLiteConnection("Data Source=./rss.db;Version=3;");
+                m_dbConnection.Open();
+                string sql = "insert or replace into JOBS (ID, SUBJECT, SUMMARY, LINK, PUBLISHDATE) values (?,?,?,?,?)";
+                var command = new SQLiteCommand(sql, m_dbConnection);
+                command.Parameters.AddWithValue("ID", ID);
+                command.Parameters.AddWithValue("subject", subject);
+                command.Parameters.AddWithValue("summary", summary);
+                command.Parameters.AddWithValue("links", links);
+                command.Parameters.AddWithValue("publishDate", publishDate);
+                command.ExecuteNonQuery();
+            }
+            //while (true)
+            //{
+            //    Console.ReadKey();
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
